1a9dac53e86a06ef5ad154d0abd6a5009da443bc
Seydou Dia <seydou.dia@olx.com>
2018-05-21 21:57:48 +0200

    got POST working fine

--- fixtures.exs
+++ fixtures.exs
@@ -3,79 +3,25 @@ use Database
 
 
 Amnesia.transaction do
-  john = %User{name: "John", email: "john@example.com"} |> User.write
-  richard = %User{name: "Richard", email: "richard@example.com"} |> User.write
-  linus   = %User{name: "Linus", email: "linus@example.com"} |> User.write
 
 
-
-  john |> User.add_message( """
-  When we program a computer to make choices intelligently after determining
-  its options, examining their consequences, and deciding which is most
-  favorable or most moral or whatever, we must program it to take an attitude
-  towards its freedom of choice essentially isomorphic to that which a human
-  must take to his own.
-  """)
-
-  john |> User.add_message("""
-  He who refuses to do arithmetic is doomed to talk nonsense."
-  """)
-
-  john |> User.add_message("""
-  It's difficult to be rigorous about whether a machine really 'knows',
-  'thinks', etc., because we're hard put to define these things. We understand
-  human mental processes only slightly better than a fish understands swimming.
-  """)
-
-  richard |> User.add_message( """
-  For personal reasons, I do not browse the web from my computer. (I also have
-  no net connection much of the time.) To look at page I send mail to a daemon
-  which runs wget and mails the page back to me. It is very efficient use of my
-  time, but it is slow in real time.
-  """)
-
-  richard |> User.add_message( """
-  I am skeptical of the claim that voluntarily pedophilia harms children. The
-  arguments that it causes harm seem to be based on cases which aren't
-  voluntary, which are then stretched by parents who are horrified by the idea
-  that their little baby is maturing.
-  """)
-
-  linus |> User.add_message( """
-  Portability is for people who cannot write new programs.
-  """)
-
-  linus |> User.add_message( """
-  Really, I'm not out to destroy Microsoft. That will just be a completely
-  unintentional side effect.
-  """)
-
-  linus |> User.add_message( """
-  Modern PCs are horrible. ACPI is a complete design disaster in every way. But
-  we're kind of stuck with it. If any Intel people are listening to this and
-  you had anything to do with ACPI, shoot yourself now, before you reproduce.
-  """)
+  variants = [
+    %{"name" => "bucket_a", "allocation" => 25, "is_control" => true,  "description" => "bucket A", "payload" => "Nada"},
+    %{"name" => "bucket_b", "allocation" => 25, "is_control" => false, "description" => "bucket B", "payload" => "Nada"},
+    %{"name" => "bucket_c", "allocation" => 50, "is_control" => false, "description" => "bucket C", "payload" => "Nada"}]
 
 
-  IO.puts(Message.count())
-  IO.puts(User.count())
+    %Experiment{name: "experiment_00", sampling: 5, description: "Experiment 00", created_date: DateTime.utc_now, updated_date: DateTime.utc_now, start_date: DateTime.utc_now, end_date: DateTime.utc_now, variants: variants} |> Experiment.write
+    %Experiment{name: "experiment_01", sampling: 5, description: "Experiment 01", created_date: DateTime.utc_now, updated_date: DateTime.utc_now, start_date: DateTime.utc_now, end_date: DateTime.utc_now, variants: variants} |> Experiment.write
+    %Experiment{name: "experiment_02", sampling: 5, description: "Experiment 02", created_date: DateTime.utc_now, updated_date: DateTime.utc_now, start_date: DateTime.utc_now, end_date: DateTime.utc_now, variants: variants} |> Experiment.write
+    %Experiment{name: "experiment_03", sampling: 5, description: "Experiment 03", created_date: DateTime.utc_now, updated_date: DateTime.utc_now, start_date: DateTime.utc_now, end_date: DateTime.utc_now, variants: variants} |> Experiment.write
+    %Experiment{name: "experiment_04", sampling: 5, description: "Experiment 04", created_date: DateTime.utc_now, updated_date: DateTime.utc_now, start_date: DateTime.utc_now, end_date: DateTime.utc_now, variants: variants} |> Experiment.write
+    %Experiment{name: "experiment_05", sampling: 5, description: "Experiment 05", created_date: DateTime.utc_now, updated_date: DateTime.utc_now, start_date: DateTime.utc_now, end_date: DateTime.utc_now, variants: variants} |> Experiment.write
+
+
 
 end
 
 
-
-Amnesia.transaction do
-#   john = User.read(18)
-#   john |> User.messages |> Enum.each(&IO.puts(&1.content))
-#   selection = Message.where true    # user_id > 1,
-#       # select: content
-#
-#   selection |> Amnesia.Selection.values |> Enum.each(&IO.puts(&1.content))
-
-  IO.puts(Message.count())
-  IO.puts(User.count())
-end
-IO.puts(Application.get_env(:mnesia, :dir))
-IO.puts(Amnesia.Helper.Options)
 
 :mnesia.dump_log()
--- lib/router.ex
+++ lib/router.ex
@@ -2,6 +2,11 @@ defmodule Abento.Router do
   use Plug.Router
   require Logger
 
+
+  use Amnesia
+  use Database
+
+
   plug(Plug.Logger)
   plug(Plug.Parsers, parsers: [:json], json_decoder: Poison)
   plug(:match)
@@ -20,34 +25,62 @@ defmodule Abento.Router do
   end
 
   post "/api/experiments" do
-    {status, body} =
-      case conn.body_params do
-        %{"experiment_label" => experiment_label} -> {200, create_experiment(experiment_label)}
-        _ -> {400, "Bad request"}
-      end
+    ## IO.puts("Routing to create_experiment...")
+    ## {status, body} =
+    ##   case conn.body_params do
+    ##     %Experiment -> {200, create_experiment(conn.body_params)}
+    ##     _ -> {400, "Bad request"}
+    ##   end
 
+    {status, body} = create_experiment(conn.body_params)
     send_resp(conn, status, body)
   end
 
   match(_, do: send_resp(conn, 404, "Not found :-)!"))
 
   defp get_experiments() do
-    data = [
-      %{"experiment_id" => 1, "label" => "button_color"},
-      %{"experiment_id" => 2, "label" => "home page update"},
-      %{"experiment_id" => 3, "label" => "backend testing"},
-      %{"experiment_id" => 4, "label" => "a/a testing"},
-      %{"experiment_id" => 5, "label" => "new layout style"}
-    ]
+    ##    data = [
+    ##      %{"experiment_id" => 1, "label" => "button_color"},
+    ##      %{"experiment_id" => 2, "label" => "home page update"},
+    ##      %{"experiment_id" => 3, "label" => "backend testing"},
+    ##      %{"experiment_id" => 4, "label" => "a/a testing"},
+    ##      %{"experiment_id" => 5, "label" => "new layout style"}
+    ##    ]
+    ##
+    ##    Poison.encode!(data)
+    Amnesia.transaction do
+      selection = Experiment.where true
+      selection |> Amnesia.Selection.values |> Poison.encode!
+    end
 
-    Poison.encode!(data)
   end
 
   defp get_experiment(id) do
     Poison.encode!(%{"experiment_id" => id, "label" => "button_color"})
   end
 
-  defp create_experiment(xp_label) do
-    Poison.encode!(%{"experiment_id" => 12, "label" => xp_label})
+  defp create_experiment(p) do
+    ##  Poison.encode!(%{"experiment_id" => 12, "label" => xp_label})
+
+    body = Amnesia.transaction do
+       %Experiment{name: p["name"],
+                   sampling: p["sampling"],
+                   description: p["description"],
+                   created_date: DateTime.utc_now,
+                   updated_date: DateTime.utc_now,
+                   start_date: p["start_date"],
+                   end_date: p["end_date"],
+                   variants: p["variants"]
+                 }
+
+
+                   |> Experiment.write
+
+
+     (Experiment.where name == p["name"])
+             |> Amnesia.Selection.values
+             |> Poison.encode!
+    end
+    {200, body}
   end
 end
